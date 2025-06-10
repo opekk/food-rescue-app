3794b0c9d81c3cb1e88560d251f03d78bd2203b7
opekk <maciek09874@gmail.com>
2025-06-11 00:26:03 +0200

    Initial Commit

---
+++ food-rescue-map/food_rescue_mapApp.swift
@@ -0,0 +1,20 @@
+//
+//  food_rescue_mapApp.swift
+//  food-rescue-map
+//
+//  Created by Maciek on 11/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct food_rescue_mapApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
